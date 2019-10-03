d5398201dd612163d912e452462d55f24ac9b136
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-04 01:20:58 +0530

    Create RailFenceCipher.java

---
+++ RailFenceCipher.java
@@ -0,0 +1,64 @@
+package railfencecipher;
+public class RailFenceCipher
+{
+    int numRails;
+    public RailFenceCipher(int numRails)
+    {
+	this.numRails = numRails;
+    }
+    String getDecryptedData(String data) 
+    {
+	char[] decrypted = new char[data.length()];
+	int n = 0;
+	for(int k = 0 ; k < numRails; k ++) 
+        {
+            int index = k;
+            boolean down = true;
+            while(index < data.length() )
+            {
+                decrypted[index] = data.charAt(n++);
+                if(k == 0 || k == numRails - 1) 
+                    index = index + 2 * (numRails - 1);
+                else if(down) {
+                    index = index +  2 * (numRails - k - 1);
+                    down = !down; }
+                else {
+                    index = index + 2 * k;
+                    down = !down; }
+            }
+        }
+        return new String(decrypted);
+    }
+    String getEncryptedData(String data) 
+    {
+	char[] encrypted = new char[data.length()];
+	int n = 0;
+	for(int k = 0 ; k < numRails; k ++) 
+        {
+            int index = k;
+            boolean down = true;
+            while(index < data.length() ) 
+            {
+		encrypted[n++] = data.charAt(index);
+		if(k == 0 || k == numRails - 1) 
+                    index = index + 2 * (numRails - 1);
+		else if(down) {
+                    index = index +  2 * (numRails - k - 1);
+                    down = !down; }
+		else {
+                    index = index + 2 * k;
+                    down = !down; }
+            }
+	}
+        return new String(encrypted);
+    }
+    public static void main(String[] args) 
+    {
+        String data = "GEEKSFORGEEKS";
+	RailFenceCipher railFenceCipher = new RailFenceCipher(2);
+	String encrypted =railFenceCipher.getEncryptedData(data);
+	System.out.println("encryption = "+encrypted);
+	String decrypted = railFenceCipher.getDecryptedData(encrypted);
+	System.out.println("decryption = "+decrypted);
+    }
+}
