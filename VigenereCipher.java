e62fa087cad84f4bc1e64a1b20145d5c27f9877b
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-04 01:23:05 +0530

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -12,22 +12,19 @@ public class VigenereCipher
         char[] plainTextChar = plainText.toUpperCase().toCharArray();
         char[] keyChar = Arrays.copyOf(key.toUpperCase().toCharArray(),plainTextChar.length);
         int i=0;
-        //Making length of Key same as length of Plain Text message
         for(int j=key.toCharArray().length;j<keyChar.length;j++)
         {
             keyChar[j]=keyChar[i];
             i++;
         }
         char[] cipherTextChar = new char[keyChar.length];
-        //Encryption
-        for(i=0;i<cipherTextChar.length;i++)
+        for(i=0;i<cipherTextChar.length;i++)//Encryption
         {
             cipherTextChar[i]=(char)(int) ((plainTextChar[i]+keyChar[i]-130)%26+65);
         }
         System.out.println("Cipher Text="+String.valueOf(cipherTextChar));
         char[] recoveredPlainTextChar = new char[cipherTextChar.length];
-        //Decryption
-        for(i=0;i<recoveredPlainTextChar.length;i++)
+        for(i=0;i<recoveredPlainTextChar.length;i++)//Decryption
         {
             recoveredPlainTextChar[i] = (char)(int) ((cipherTextChar[i]-keyChar[i]+26)%26+65);
         }
