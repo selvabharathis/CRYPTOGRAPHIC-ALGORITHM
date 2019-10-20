d4b97a1f6ceb3e5abf57ece7068e786783802fde
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-20 23:49:10 +0530

    Update MessageDigest_Algorithm.java

--- MessageDigest_Algorithm.java
+++ MessageDigest_Algorithm.java
@@ -1,1 +1,26 @@
-
+package md5;
+import com.sun.crypto.provider.SunJCE;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.Security;
+public class MD5 
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Security.addProvider(new SunJCE());
+        System.out.println("Enter the message");
+        String message=br.readLine();
+        byte[] input = message.getBytes();
+        System.out.println("Message: " + new String(input));
+        MessageDigest md = MessageDigest.getInstance("MD5");     // Message Digest 5 Algorithm
+        //MessageDigest md = MessageDigest.getInstance("SHA-1"); // SHA 1 Algorithm
+        md.update(input);
+        byte[] hash = md.digest();
+        BigInteger b=new BigInteger(1,hash);
+        String hashValue=b.toString(16);
+        System.out.println("MessageDigest in HexaDecimal Format: " + hashValue);
+    }
+}
