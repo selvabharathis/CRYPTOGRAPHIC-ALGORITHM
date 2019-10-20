3633dcdd611f0fa78f504436121b67239ce12a8d
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-20 23:41:19 +0530

    Update RSA_Algorithm.java

--- RSA_Algorithm.java
+++ RSA_Algorithm.java
@@ -1,1 +1,27 @@
-
+package rsa;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+public class RSA 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Encryption Constant:");
+        BigInteger e = new BigInteger(br.readLine());
+        System.out.println("Enter Prime Number 1:");
+        BigInteger p = new BigInteger(br.readLine());
+        System.out.println("Enter Prime Number 2:");
+        BigInteger q = new BigInteger(br.readLine());
+        BigInteger n = p.multiply(q);
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger d = e.modInverse(phi);
+        System.out.println("Enter PlainText Message:");
+        BigInteger plainText = new BigInteger(br.readLine());
+        BigInteger cipherText = plainText.modPow(e, n);
+        System.out.println("CipherText:" + cipherText);
+        BigInteger recoveredPlainText = cipherText.modPow(d, n);
+        System.out.println("Recovered PlainText:" + recoveredPlainText);
+    }
+}
