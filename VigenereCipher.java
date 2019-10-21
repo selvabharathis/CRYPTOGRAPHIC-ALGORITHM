9aa45956de1ed562df49b165f5e921a40603294d
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-22 01:52:55 +0530

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -19,15 +19,11 @@ public class VigenereCipher
         }
         char[] cipherTextChar = new char[keyChar.length];
         for(i=0;i<cipherTextChar.length;i++)//Encryption
-        {
-            cipherTextChar[i]=(char)(int) ((plainTextChar[i]+keyChar[i]-130)%26+65);
-        }
+            cipherTextChar[i] = (char)(((int)plainTextChar[i] + (int)keyChar[i] - 130) % 26 + 65);// converting 2 ASCII value to normal index so -65 -65 = -130
         System.out.println("Cipher Text="+String.valueOf(cipherTextChar));
         char[] recoveredPlainTextChar = new char[cipherTextChar.length];
         for(i=0;i<recoveredPlainTextChar.length;i++)//Decryption
-        {
-            recoveredPlainTextChar[i] = (char)(int) ((cipherTextChar[i]-keyChar[i]+26)%26+65);
-        }
+            recoveredPlainTextChar[i] = (char)(((int)cipherTextChar[i] - (int)keyChar[i] + 26) % 26 + 65);// we doesnot need and value to make it normal, since ASCII with another ASCII subtraction handle them self with same result, +26 is added since some value may exceed/deeced the parameter of mod26(when negative value is found during subtraction).  
         System.out.println("Recovered Plain Text="+String.valueOf(recoveredPlainTextChar).toLowerCase());
     }
 }
