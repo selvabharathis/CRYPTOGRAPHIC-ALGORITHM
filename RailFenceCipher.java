a0c2e92066272ad24f5c2af311e1d6ac34e4a935
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2019-10-22 01:48:33 +0530

    Update RailFenceCipher.java

--- RailFenceCipher.java
+++ RailFenceCipher.java
@@ -13,18 +13,11 @@ public class RailFenceCipher
 	for(int k = 0 ; k < numRails; k ++) 
         {
             int index = k;
-            boolean down = true;
             while(index < data.length() )
             {
                 decrypted[index] = data.charAt(n++);
                 if(k == 0 || k == numRails - 1) 
                     index = index + 2 * (numRails - 1);
-                else if(down) {
-                    index = index +  2 * (numRails - k - 1);
-                    down = !down; }
-                else {
-                    index = index + 2 * k;
-                    down = !down; }
             }
         }
         return new String(decrypted);
@@ -36,18 +29,11 @@ public class RailFenceCipher
 	for(int k = 0 ; k < numRails; k ++) 
         {
             int index = k;
-            boolean down = true;
             while(index < data.length() ) 
             {
 		encrypted[n++] = data.charAt(index);
 		if(k == 0 || k == numRails - 1) 
                     index = index + 2 * (numRails - 1);
-		else if(down) {
-                    index = index +  2 * (numRails - k - 1);
-                    down = !down; }
-		else {
-                    index = index + 2 * k;
-                    down = !down; }
             }
 	}
         return new String(encrypted);
